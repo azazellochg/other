2349ff5201b329140bab8222fcff5cc3008165d0
Grigory Sharov <sharov.grigory@gmail.com>
2015-04-10 17:06:56 +0200

    Update check.sh

--- movie/check.sh
+++ movie/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# this script check if every mrc file has a pair (frames and total)
 #since sometimes total exposure is registered faster than frame stack, for each file without pair the script will look for total exposure file
 #registered 1 minute before (timestamp in filename), i.e. for *2358_frames.mrc it will check for *2357.mrc
+# then this script check if every mrc file has a pair (frames and total)
 
 if [ "$#" -ne 1 ] || [ "$1" != "mrc" ]; then
 echo "usage: `basename $0` mrc" # Right now it is not working with tifs
@@ -13,20 +13,16 @@ fi
 [ ! -d logs ] && mkdir logs
 [ ! -f logs/del.list ] && find . -name "FoilHole*.${1}" | sed -rn 's/FoilHole_[0-9]*_Data_[0-9]*_([^\.]+)\.'$1'/\1\t\0/p' | sort | cut -f2 > logs/del.list
 
-sed 's/_frames//g;s/.mrc//g' logs/del.list | uniq > logs/del.list2
-while read filename;
-        do
-        [ -f ${filename}.mrc -a -f ${filename}_frames.mrc ] || ( echo "Check file: ${filename}*.mrc" && echo "${filename}" >> logs/bad_files.txt )
-        done < logs/del.list2
-echo "Bad files are in: logs/bad_files.txt"
-
-for ima in `cat logs/bad_files.txt`
+for ima in `grep frames logs/del.list | sed 's/_frames.mrc//g'`
 do
 base=`echo $ima | awk -F "_" '{$NF=""}1' |  sed -e "s/ /_/g" -e "s/_$//"`
 HourMin=`echo $ima| awk -F "_" '{print $(NF)}'`
 if [[ "$HourMin" == 0000 ]]
 then
 RealNum=2359
+date_old=`echo "${base:(-8)}"`
+date_new=`date "--date=${date_old} -1 day" +%Y%m%d`
+base=`echo ${base} | sed -r 's/.{8}$/'$date_new'/'`
 else
 Min=${HourMin:2:2}
 Hours=${HourMin:0:2}
@@ -38,11 +34,7 @@ fi
 if [ ! -f ${ima}.mrc ]
 then
 HiDo=${base}_${RealNum}
-[ -f ${HiDo}.mrc ] && echo "time stamp shift detected!"
-echo "hido=${HiDo}"
-echo "ima=${ima}"
-#use mv only if necessary!
-#mv ${HiDo}.mrc ${ima}.mrc
+[ -f ${HiDo}.mrc ] && echo "time stamp shift detected, real total exposure file = ${HiDo}.mrc" && echo -e "                           corresponding frame stack = ${ima}_frames.mrc\n" && mv ${HiDo}.mrc ${ima}.mrc
 elif [ -f ${ima}.mrc ]
 then
 HiDo=$ima
@@ -51,3 +43,10 @@ echo $ima >> logs/suspicious_frames.plt
 continue
 fi
 done
+
+sed 's/_frames//g;s/.mrc//g' logs/del.list | uniq > logs/del.list2
+while read filename;
+        do
+        [ -f ${filename}.mrc -a -f ${filename}_frames.mrc ] || ( echo "Check file: ${filename}*.mrc" && echo "${filename}" >> logs/bad_files.txt )
+        done < logs/del.list2
+echo "Bad files are in: logs/bad_files.txt"
