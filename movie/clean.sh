f2a48703363c6305c00756e169f2942f21bd1472
Grigory Sharov <sharov.grigory@gmail.com>
2015-07-30 13:32:16 +0200

    Update clean.sh

--- movie/clean.sh
+++ movie/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # clean the folders after EPU acquisition, removing all xml | jpg | dm and all metadata
 
-find . \( -name "*.xml" -o -name "*.jpg" -o -name "*.dm" -o -name "FoilHoles" -o -name "Metadata" -o -name "GridSquare*" \) -exec rm -rf {} +
+find . \( -name "*.xml" -o -name "*.jpg" -o -name "*.dm" -o -name "FoilHoles" -o -name "Metadata" \) >> bad_files.txt
