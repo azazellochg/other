41c79a640906a854660bf14772a6595c20382e51
Gregory Sharov <sharov.grigory@gmail.com>
2016-01-10 19:43:27 +0100

    added conversion to 16-bit after motioncorr

--- movie/movie_align.sh
+++ movie/movie_align.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-###Run movie_prep_parallel.sh first!
+###Run movie_prep_parallel.sh first and source your EMAN2!
 #source /home/sharov/soft/EMAN2.1/eman2.bashrc
+if [ -z $EMAN2DIR ]; then
+        echo "EMAN2 NOT found!" && exit 1
+fi
 trap 'kill -HUP -$$' exit; nvidia-smi -l 300 >/dev/null & # by Jonathan
 movie_soft_path="/usr/local/bin"
 [ -d aligned_sums ] && rm -rf aligned_sums
@@ -9,40 +12,44 @@ mkdir aligned_movies
 mkdir aligned_sums
 [ -d logs/alignment ] && rm -rf logs/alignment
 mkdir -p logs/alignment
-[ ! -d raw_stacks ] && echo "No stacks found. Exiting.." && exit 1
+[ ! -d raw_stacks ] && echo "No raw stacks found. Exiting.." && exit 1
 [ ! -s logs/frame.list ] && echo "No frame list found. Exiting.." && exit 1
 echo -n "Do you want to save aligned movie stacks? (0 - no, 1 - yes, default: 1): "
-read ssc
+read ssc 
 ssc=${ssc:-1}
 re='^[0-1]+$'
 if ! [[ $ssc =~ $re ]]; then
-echo "error: Wrong answer!" >&2; exit 1
+        echo "error: Wrong answer!" >&2; exit 1
 fi
 total=`wc -l < logs/frame.list`
 key=1
 echo ""
 for stack in `cat logs/frame.list | sed 's/.*FoilHole/FoilHole/g;s/_frames.mrc//g'`
 do
-if [ -f raw_stacks/${stack}_stack.mrcs ] && [ ! -f aligned_sums/${stack}.mrc ]
-then
-echo -ne "Aligning frames $key/$total: ...\r"
-timeout 1.5m ${movie_soft_path}/motioncorr_v2.1 raw_stacks/${stack}_stack.mrcs -fod 2 -ssc $ssc -fct aligned_movies/${stack}_movie.mrcs -fcs aligned_sums/${stack}.mrc -dsp 0 -atm 1 -flg logs/alignment/${stack}_align.log &>/dev/null
-if [ ! -f aligned_sums/${stack}.mrc ]
-then
-echo "raw_stacks/${stack}_stack.mrcs" >> logs/not_aligned.plt
-echo -ne "Aligning frames $key/$total: ... FAIL!\n"
-else
-shift=`grep "Final shift (Average" logs/alignment/${stack}_align.log | sed -e 's/Final\ shift\ (Average//g;s/)\://g'`
-echo -ne "Aligning frames $key/$total: ... average shift ${shift}\n"
-fi
-fi
-((key++))
+        if [ -f raw_stacks/${stack}_stack.mrcs ] && [ ! -f aligned_sums/${stack}.mrc ]
+        then
+                echo -ne "Aligning frames $key/$total: ...\r"
+                timeout 1.5m ${movie_soft_path}/motioncorr_v2.1 raw_stacks/${stack}_stack.mrcs -fod 2 -ssc $ssc -fct aligned_movies/${stack}_movie.mrcs -fcs aligned_sums/${stack}.mrc -dsp 0 -atm 1 -flg logs/alignment/${stack}_align.log &>/dev/null
+                if [ ! -f aligned_sums/${stack}.mrc ]
+                then
+                        echo "raw_stacks/${stack}_stack.mrcs" >> logs/not_aligned.plt
+                        echo -ne "Aligning frames $key/$total: ... FAIL!\n"
+                else
+                        shift=`grep "Final shift (Average" logs/alignment/${stack}_align.log | sed -e 's/Final\ shift\ (Average//g;s/)\://g'`
+                        echo -ne "Aligning frames $key/$total: ... average shift ${shift}\n"
+                        e2proc2d.py aligned_sums/${stack}.mrc aligned_sums/${stack}.mrc --outmode=int16 > /dev/null 2>&1
+                        e2proc2d.py aligned_movies/${stack}_movie.mrcs aligned_movies/${stack}_movie_16bit.mrcs --outmode=int16 > /dev/null 2>&1
+                        mv aligned_movies/${stack}_movie_16bit.mrcs aligned_movies/${stack}_movie.mrcs
+                fi  
+        fi  
+        ((key++))
 done
 grep "Final shift (Average" logs/alignment/* | sed -e 's/Final\ shift\ (Average//g;s/)\://g' > logs/average_shift1.log
 cat logs/average_shift1.log | sort -n -k2 > logs/average_shift.log
 rm -f logs/average_shift1.log
-echo -e "\nResults: shifts in -> logs/average_shift.log
+echo -e "\nResults:  shifts in -> logs/average_shift.log
           detailed logs in -> logs/alignment/*.log
           NOT aligned images in -> logs/not_aligned.plt
-	  aligned movies for Relion -> aligned_movies/*_movie.mrcs
-	  aligned sums for Relion -> aligned_sums/*.mrc"
+          aligned movies for Relion -> aligned_movies/*_movie.mrcs
+          aligned sums for Relion -> aligned_sums/*.mrc"
+echo "Do not forget to move CTFFIND4 output files from Micrographs/ to your aligned_sums/ folder!"
