0b557d37e9bf208931954787f330626b8902bb65
Gregory Sharov <sharov.grigory@gmail.com>
2016-01-21 15:34:34 +0100

    version 2

--- picking-msa/prepare-files.sh
+++ picking-msa/prepare-files.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# This script converts the output from gempicker run to relion star file (coordinates.star),
-# then combines it with star file from Relion (extracted particles) into particles_new.star
-# and creates IMAGIC stacks for both particles and references.
-# For gAutomatch, it only creates IMAGIC stacks for particles and references.
-
+# This script adds output from gempicker run to relion star file (into particles_new.star).
+# After gAutomatch picking user already has a star file with all the info.
+# In both cases, the script creates IMAGIC stacks for both particles and references, preparing them for MSA run.
+#
+# User should have done particle extraction in Relion using coordinates obtained from gEMpicker/gAutomatch.
+# Folder with picking results (see below), references and extracted particle stacks are needed (usually "Particles" folder in Relion)
+#
 ###### USER INPUT #######################################
 particles_extracted="particles.star"                    # input particle star file after extraction in relion
-output="Micrographs"                                    # output folder with picking results (gEMpicker: contains pik_coord folder | gAutomatch: contains *_gautopick.star files)
+output="Micrographs"                                    # folder with picking results (gEMpicker: contains pik_coord folder | gAutomatch: contains *_gautopick.star files)
 
 references="proj_for_pick_c4_new.mrcs"                  # specify only for gAutomatch: references stack (*.mrcs)
 
 coarse="4"                                              # specify only for gEMpicker: N times mics were coarsed before gEMpicker run
 coarsed_micsize="1024"                                  # specify only for gEMpicker: size of coarsed mics during gEMpicker run
 particles_new="particles_new.star"                      # specify only for gEMpicker: output particle star file with gempicker info
-coordinates="coordinates.star"                          # specify only for gEMpicker: output coordinate star file name to be created (no need to change)
 
+coordinates="coordinates.star"                          # DO NOT CHANGE! only for gEMpicker: output coordinate star file name to be created
 debug="1"                                               # if 0 - remove intermediate files
 #########################################################
 
@@ -25,8 +27,17 @@ fi
 if [ -z $EMAN2DIR ]; then
         echo "EMAN2 NOT found!" && exit 1
 fi
-[ ! -f ${particles_extracted} ] && echo "${particles_extracted} file not found! Did you extract particles in Relion?" && exit 1
 
+# check input
+[ ! -f ${particles_extracted} ] && echo "${particles_extracted} file not found! Did you extract particles in Relion?" && exit 1
+micField=`grep "_rlnMicrographName" ${particles_extracted} | cut -d'#' -f2`
+ptclField=`grep "_rlnImageName" ${particles_extracted} | cut -d'#' -f2`
+if [ ! -f `awk -v micField=$micField 'NF>3{print $micField}' ${particles_extracted} | head -n1` ] || [ ! -f `awk -v ptclField=$ptclField 'NF>3{print $ptclField}' ${particles_extracted} | head -n1 | cut -d'@' -f2` ]
+then
+        echo "ERROR! Micrographs or particles, specified in input ${particles_extracted} file, not found. Check your star file!" && exit 1
+fi
+
+### BEGIN gEMpicker_parse function
 gEMpicker_parse() {
 # gEMpicker txt file: $2=CC, $3=X, $4=Y, $5=box, $6=ref#, $7=angle
 # txt to box: (X;Y)-->(micsize-Y-box/2,X-box/2+1)
@@ -87,12 +98,15 @@ check_star() {
 # add new fields to particles_extracted star file
 awk '{print $4,$5,$6}' tmp/${coordinates} > tmp/${coordinates}.tmp
 pr -mJt -s' ' tmp/${particles_extracted} tmp/${coordinates}.tmp >> ${particles_new}
-echo "New particle star file created: ${particles_new}"
+echo "New particle star file with gEMpicker info created: ${particles_new}"
 particles_new=`echo ${particles_new} | sed 's/.star//'`
 }
+### END gEMpicker_parse function
 
+### BEGIN gAutomatch_parse function
 gAutomatch_parse() {
 # renumber references
+echo "Acquiring results from gAutomatch..."
 refNo=`grep "_rlnClassNumber" ${particles_extracted} | cut -d'#' -f2`
 if [ `awk -v refNo=$refNo '$refNo==0{print $refNo}' ${particles_extracted} | wc -l` -ne 0 ];then
         awk -v refNo=$refNo 'NF<3||$refNo=$refNo+1' ${particles_extracted} > ${particles_extracted}.tmp
@@ -107,6 +121,7 @@ else
         exit 1
 fi
 }
+### END gAutomatch_parse function
 
 if [ ! -d ${output}/pik_coord ]; then
         gAutomatch_parse
@@ -125,8 +140,11 @@ e2proc2d.py ${particles_new}.mrcs ${particles_new}.img
 echo "Creating plt file with particleID, CCC and refID..."
 field2=`grep "_rln" ${particles_new}.star | wc -l`
 field1=$((field2-1))
-awk -v a=$field1 -v b=$field2 'NF>3{print $a,$b}' ${particles_new}.star |nl > ${particles_new}.plt
+awk -v a=$field1 -v b=$field2 'NF>3{print $a,$b}' ${particles_new}.star | nl > ${particles_new}.plt
 
-[ $debug -eq 0 ] && rm -rf tmp ${particles_new}.mrcs picking_references.mrcs
-echo "SUCCESS! Output stack of (aligned) particles: ${particles_new}.img
-You also have picking_references.img stack for further analysis."
+# clean up
+[ $debug -eq 0 ] && rm -rf tmp ${particles_new}.mrcs picking_references.mrcs 
+echo "DONE! Output files:
+--> Stack of aligned particles: ${particles_new}.img
+--> File with particleID, CCC and refID: ${particles_new}.plt
+--> Stack of picking references: picking_references.img"
