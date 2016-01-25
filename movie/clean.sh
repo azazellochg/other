fa9139a0d1eaecfc7acac938bcc9789b6676c3cd
Gregory Sharov <sharov.grigory@gmail.com>
2016-01-25 12:18:00 +0100

    correct typo

--- movie/clean.sh
+++ movie/clean.sh
@@ -2,4 +2,4 @@
 # clean the folders after EPU acquisition, removing all xml | jpg | dm and all metadata
 [ -f bad_files.txt ] && rm -f bad_files.txt
 find . \( -name "*.xml" -o -name "*.jpg" -o -name "*.dm" -o -name "FoilHoles" -o -name "Metadata" \) >> bad_files.txt
-[ -s bad_files.txt ] && "echo "Found some metadata files: see bad_files.txt"
+[ -s bad_files.txt ] && echo "Found some metadata files: see bad_files.txt"
