7112532dc194543fb364d0855abc5cf33ece2fb5
Gregory Sharov <sharov.grigory@gmail.com>
2016-07-12 16:19:40 +0200

    fix movie input file name

--- movie/motioncor2.sh
+++ movie/motioncor2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #---input-parameters---
+# input movies should be in 'raw_stacks' folder
 program='/usr/local/bin/MotionCor2-03-16-2016'
 pix="1.1"
 dose="7.05"
-patch="5 5" # this does not work!!! change the line 25 yourself
+patch="5 5" # this does not work! change the line 27 yourself
 kv="300"
 group="1"
 frames="7"
@@ -15,14 +16,15 @@ mkdir -p logs/motioncor2 aligned_sums_motioncor2
 [ -f logs/avg_shift_motioncor2.log ] && rm -f logs/avg_shift_motioncor2.log
 [ -f logs/motioncor2_failed.log ] && rm -f logs/motioncor2_failed.log
 k=1
-total=`ls raw_stacks/ | wc -l`
 fr=$((frames*2))
+total=`ls raw_stacks/ | wc -l`
+suffix=`ls raw_stacks/ | head -n1 | awk -F "_" '{print $NF}'`
 echo ""
 
-for i in `ls raw_stacks/ | sed 's/raw_stacks\///g;s/_frames.mrc//g'`;
+for i in `ls raw_stacks/ | sed 's/raw_stacks\///g;s/_'$suffix'//g'`;
 do
         echo -ne "Aligning stack $k/$total..\r"
-        $program -InMrc "raw_stacks/${i}_frames.mrc" -OutMrc "aligned_sums_motioncor2/${i}_aligned_sum.mrc" -Patch 5 5 -FmDose "${dose}" -LogFile "logs/motioncor2/${i}_" -PixSize "${pix}" -kV $kv -Group $group >> logs/motioncor2.log 2>&1
+        $program -InMrc "raw_stacks/${i}_${suffix}" -OutMrc "aligned_sums_motioncor2/${i}_aligned_sum.mrc" -Patch 5 5 -FmDose "${dose}" -LogFile "logs/motioncor2/${i}_" -PixSize "${pix}" -kV $kv -Group $group >> logs/motioncor2.log 2>&1
         [ $? -ne 0 ] && echo -e "Alignment failed for the stack ${k}!\n" && echo "raw_stacks/${i}_frames.mrc" >> logs/motioncor2_failed.log
         avgshift=`sed 's/-//g' logs/motioncor2/${i}_0-Patch-Full.log | awk -v fr=$fr 'NR>3{sum+=$2;sum+=$3}END{print sum/fr}'`
         echo "raw_stacks/${i}_frames.mrc - avg frame shift: $avgshift px" >> logs/avg_shift_motioncor2.log
