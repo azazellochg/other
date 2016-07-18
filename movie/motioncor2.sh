1004634f63f23c7fe31464a28ddb09cf7248c342
Gregory Sharov <sharov.grigory@gmail.com>
2016-07-18 14:12:07 +0200

    add libs

--- movie/motioncor2.sh
+++ movie/motioncor2.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#exporting CUDA 7.5 libs
+if ! (echo "${LD_LIBRARY_PATH}" | grep "cuda-7.5" > /dev/null 2>&1)
+        then export LD_LIBRARY_PATH="/usr/local/cuda-7.5/lib64/:${LD_LIBRARY_PATH}"
+fi
+
 #---input-parameters---
 # input movies should be in 'raw_stacks' folder
 program='/usr/local/bin/MotionCor2-03-16-2016'
 pix="1.1"
-dose="7.05"
-patch="5 5" # this does not work! change the line 27 yourself
+dose="7.05" # dose per frame
+patch="5 5" # this var does not work!!! change cmd line yourself
 kv="300"
-group="1"
-frames="7"
+group="1"   # group frames
+frames="7"  # total N of frames
 #----------------------
 
 [ -d aligned_sums_motioncor2 ] && echo "Please remove aligned_sums_motioncor2 folder and restart the script!" && exit 1
 mkdir -p logs/motioncor2 aligned_sums_motioncor2
-[ -f logs/avg_shift_motioncor2.log ] && rm -f logs/avg_shift_motioncor2.log
+[ -f logs/motioncor2_avg_shift.log ] && rm -f logs/motioncor2_avg_shift.log
 [ -f logs/motioncor2_failed.log ] && rm -f logs/motioncor2_failed.log
 k=1
 fr=$((frames*2))
@@ -27,9 +32,9 @@ do
         $program -InMrc "raw_stacks/${i}_${suffix}" -OutMrc "aligned_sums_motioncor2/${i}_aligned_sum.mrc" -Patch 5 5 -FmDose "${dose}" -LogFile "logs/motioncor2/${i}_" -PixSize "${pix}" -kV $kv -Group $group >> logs/motioncor2.log 2>&1
         [ $? -ne 0 ] && echo -e "Alignment failed for the stack ${k}!\n" && echo "raw_stacks/${i}_frames.mrc" >> logs/motioncor2_failed.log
         avgshift=`sed 's/-//g' logs/motioncor2/${i}_0-Patch-Full.log | awk -v fr=$fr 'NR>3{sum+=$2;sum+=$3}END{print sum/fr}'`
-        echo "raw_stacks/${i}_frames.mrc - avg frame shift: $avgshift px" >> logs/avg_shift_motioncor2.log
+        echo "raw_stacks/${i}_${suffix} $avgshift px" >> logs/motioncor2_avg_shift.log
         ((k++))
-done
-sort -n -k2 logs/avg_shift_motioncor2.log > .tmp_sort
-mv .tmp_sort logs/avg_shift_motioncor2.log
+done        
+sort -n -k2 logs/motioncor2_avg_shift.log > .tmp_sort
+mv .tmp_sort logs/motioncor2_avg_shift.log
 echo -e "\nDone!\nLog files are in logs/motioncor2/ folder."
